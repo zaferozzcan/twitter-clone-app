9d860e83e1138c204ddcc1fcdbeba02ea7925fe1
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-26 14:09:23 -0400

    add more icon and search bar option

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import SidebarOption from "./SidebarOption/SidebarOption";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import HomeIcon from "@material-ui/icons/Home";
-import SidebarOption from "./SidebarOption/SidebarOption";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import "./StyleSidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <TwitterIcon />
-
-      <SidebarOption icon={<HomeIcon />} />
+      <SidebarOption text={"Home"} icon={<HomeIcon />} />
+      <SidebarOption text={"Explore"} icon={<SearchIcon />} />
+      <SidebarOption text={"Notifications"} icon={<NotificationsNoneIcon />} />
+      <SidebarOption icon={<MailOutlineIcon />} text="Messages" />
+      <SidebarOption icon={<BookmarkBorderIcon />} text="Bookmarks" />
+      <SidebarOption icon={<ListAltIcon />} text="Lists" />
+      <SidebarOption icon={<PermIdentityIcon />} text="Profile" />
+      <SidebarOption icon={<MoreHorizIcon />} text="More" />
     </div>
   );
 }
--- src/components/Sidebar/SidebarOption/SidebarOption.jsx
+++ src/components/Sidebar/SidebarOption/SidebarOption.jsx
@@ -3,10 +3,9 @@ import React from "react";
 export default function SidebarOption(props) {
   console.log(props);
   return (
-    <div className="sidebar-option">
-      {/* <div className="sidebar-item"> */}
-      {props.icon}
-      {/* </div> */}
-    </div>
+    <>
+      <div className="sidebarOption">{props.icon}</div>
+      <h3>{props.text}</h3>
+    </>
   );
 }
