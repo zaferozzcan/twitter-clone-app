f1776719cb0c8655cc9cbbc8738255b57739bdc3
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-26 15:14:24 -0400

    start feed component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Feed from "./components/Feed/Feed";
 
 function App() {
   return (
     <div className="App">
+      {/* Sidebar */}
       <Sidebar />
+      {/* Feed */}
+      <Feed />
     </div>
   );
 }
