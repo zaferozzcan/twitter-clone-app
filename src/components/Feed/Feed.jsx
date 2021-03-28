69ef5de6754517c64ffbe154f6df0405869b588d
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-03-28 15:33:46 -0400

    can get the data ffrom database =

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import TweetBox from "./TweetBox";
+import db from "../../firebase";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+  console.log(posts);
   return (
     <div className="feed">
       <div className="feed__header">
